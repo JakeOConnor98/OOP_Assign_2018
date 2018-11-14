f4323898177e50677b09eff6430279c777d902b1
JakeOConnor98 <Jake.OConnor@students.ittralee.ie>
2018-11-14 10:38:04 +0000

    Commit 2

--- src/LibraryGUI.java
+++ src/LibraryGUI.java
@@ -6,37 +6,40 @@ import java.awt.event.ActionListener;
 public class LibraryGUI extends JFrame implements ActionListener {
 
     JMenu fileMenu;
-    JMenu adminMenu;
+    JMenu bookMenu;
+    JMenu customerMenu;
 
+    public static void main(String[] args) {
+        LibraryGUI lGUI = new LibraryGUI();
+        lGUI.setVisible(true);
+    } //end main
 
     public LibraryGUI() {
+        Container cPane;
+
         setTitle("Library Management System");
         setSize(500, 500);
         setBackground(Color.WHITE);
         setResizable(false);
         setLocation(250, 200);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JButton jbutton = new JButton("Add Books");
-        jbutton.addActionListener(this);
+
+        cPane = getContentPane();
+        cPane.setLayout(new FlowLayout());
+
+        //JButton jbutton = new JButton("Add Books");
+        //jbutton.addActionListener(this);
         Container jpane = getContentPane();
-        jpane.add(jbutton);
+        //jpane.add(jbutton);
 
         createFileMenu();
+        createBookMenu();
 
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         menuBar.add(fileMenu);
+        menuBar.add(bookMenu);
 
-        JMenu menu, submenu;
-        JMenuItem menuItem;
-        JRadioButtonMenuItem rbMenuItem;
-        JCheckBoxMenuItem cbMenuItem;
-
-        //Create the menu bar.
-        menuBar = new JMenuBar();
-
-        //Build the first menu.
-        menu = new JMenu("A Menu");
 
     } //end constructor
 
@@ -50,22 +53,61 @@ public class LibraryGUI extends JFrame implements ActionListener {
         fileMenu.setBackground(Color.white);
 
         // create the first item
-        item = new JMenuItem("Exit");        //New
+        item = new JMenuItem("Save");
         // make sure the program is listening for clicks: handle them in 'this' class
         item.addActionListener(this);
         // attach the item to the menu
         fileMenu.add(item);
+
+
+        item = new JMenuItem("Exit");        //New
+
+        item.addActionListener(this);
+
+        fileMenu.add(item); }
+
+    public void createBookMenu(){
+        JMenuItem item; // declare a re-usable JMenuItem object
+
+        // first, create the menu: then you can start on the items
+        bookMenu = new JMenu("Book");
+
+        fileMenu.setBackground(Color.white);
+
+        // create the first item
+        item = new JMenuItem("Add");
+        item.addActionListener(this);
+        bookMenu.add(item);
+
+        item = new JMenuItem("Amend");
+        item.addActionListener(this);
+        bookMenu.add(item);
+
+        item = new JMenuItem("Delete");
+        item.addActionListener(this);
+        bookMenu.add(item);
+
+        item = new JMenuItem("Reserve");
+        item.addActionListener(this);
+        bookMenu.add(item);
+
     }
-    public static void main(String[] args) {
-        LibraryGUI lGUI = new LibraryGUI();
-        lGUI.setVisible(true);
-    } //end main
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(null, "Hi there");
+        String menuName;
+        String myBook;
+        menuName = e.getActionCommand();
+        myBook = e.getActionCommand();// what's written on the item that was clicked
+        // note the String comparison
+        if (menuName.equals("Quit")) {
+            System.exit(0);
+        } // end if
+        else if (menuName.equals("Add")){
+            JOptionPane.showInputDialog("Please enter name of book");
 
-    } //end actionPerformed
-
-
+        }
+    }
 }//end class

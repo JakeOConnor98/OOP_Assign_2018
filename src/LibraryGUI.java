1dd52f5d9597ed9182494f78a1fe766ef8a85b8b
JakeOConnor98 <Jake.OConnor@students.ittralee.ie>
2018-11-25 17:10:21 +0000

    Commit 8

--- src/LibraryGUI.java
+++ src/LibraryGUI.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.rmi.Naming;
+import java.util.ArrayList;
 
 public class LibraryGUI extends JFrame implements ActionListener {
 
@@ -149,12 +151,15 @@ public class LibraryGUI extends JFrame implements ActionListener {
         customerMenuName = e.getActionCommand();
 
         if (fileMenuName.equals("Open")){
-
+            LibraryMS.OpenBooks();
+            LibraryMS.OpenCustomers();
 
         }
 
         else if (fileMenuName.equals("Save")){
             JOptionPane.showConfirmDialog(null, "Are you sure you want to save details?");
+            //LibraryMS.SaveBooks();
+            //LibraryMS.SaveCustomers();
 
 
         }
@@ -183,6 +188,9 @@ public class LibraryGUI extends JFrame implements ActionListener {
                 if (true) {
 
                   addBookFrame();
+                  Book b = new Book();
+                  LibraryMS.addBook(b);
+
 
                 }
              }
@@ -238,7 +246,85 @@ public class LibraryGUI extends JFrame implements ActionListener {
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
         JPanel jp = new JPanel();
+        jp.setLayout(new GridLayout(0, 1));
         jFrame.add(jp);
+        JLabel title = new JLabel("Title");
+        title.setPreferredSize( new Dimension( 10, 15) );
+        title.setVerticalAlignment(JLabel.BOTTOM);
+        jp.add(title);
+        JTextField jtf = new JTextField();
+        jtf.setPreferredSize(new Dimension(50,10));
+        //jtf.setBounds(110,130,80,40);
+        jtf.addActionListener(this);
+        jp.add(jtf);
+
+
+        JLabel author = new JLabel("Author");
+        author.setVerticalAlignment(JLabel.BOTTOM);
+        jp.add(author);
+        jtf = new JTextField();
+        author.setPreferredSize( new Dimension( 10, 15));
+
+        jtf.addActionListener(this);
+        jp.add(jtf);
+
+
+        JLabel isbn = new JLabel("ISBN");
+        jp.add(isbn);
+        jtf = new JTextField();
+        isbn.setPreferredSize( new Dimension( 10, 15));
+        jtf.addActionListener(this);
+        jp.add(jtf);
+
+        JButton addBook = new JButton("Add Book");
+
+        jp.add(addBook);
+
+        if(addBook.getModel().isPressed()){
+            JOptionPane.showConfirmDialog(null, "Are you sure you want to add a book?", "Are You Sure", JOptionPane.YES_NO_OPTION);
+
+            //addBook.add(b);
+
+        }
+
+        /*JMenuItem item; // declare a re-usable JMenuItem object
+
+        // first, create the menu: then you can start on the items
+        fileMenu = new JMenu("File");
+
+        fileMenu.setBackground(Color.white);
+
+        // create the first item
+        item = new JMenuItem("Exit");
+        item.addActionListener(this);
+        fileMenu.add(item);
+
+        if (fileMenu.equals("Exit")){
+            JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?");
+
+        }*/
+
+
+
+
+    }
+
+    private void amendBookFrame() {
+        JFrame jFrame = new JFrame();
+        String header = "Amend Book";
+        jFrame.setTitle(header);
+        jFrame.setSize(500,500);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+
+
+
+        JPanel jp = new JPanel();
+        jFrame.add(jp);
+        JComboBox bookList = new JComboBox<Book>();
+        bookList.addActionListener(this);
+        bookList.add(jp);
+        //bookList.setVisible(true);
         JLabel title = new JLabel("Title");
         title.setVerticalAlignment(JLabel.BOTTOM);
         jp.add(title);
@@ -261,48 +347,18 @@ public class LibraryGUI extends JFrame implements ActionListener {
         JLabel isbn = new JLabel("ISBN");
         jp.add(isbn);
         jtf = new JTextField();
-        jtf.setBounds(110,130,80,40);
+        //jtf.setBounds(110,130,80,40);
         jtf.addActionListener(this);
         jp.add(jtf);
 
-        JButton addBook = new JButton("Add Book");
+        JButton amendBook = new JButton("Amend Book");
 
-        jp.add(addBook);
+        jp.add(amendBook);
 
-        if(addBook.getModel().isPressed()){
-            JOptionPane.showConfirmDialog(null, "Are you sure you want to add a book?", "Are You Sure", JOptionPane.YES_NO_OPTION);
-        }
-
-        JMenuItem item; // declare a re-usable JMenuItem object
-
-        // first, create the menu: then you can start on the items
-        fileMenu = new JMenu("File");
-
-        fileMenu.setBackground(Color.white);
-
-        // create the first item
-        item = new JMenuItem("Exit");
-        item.addActionListener(this);
-        fileMenu.add(item);
-
-        if (fileMenu.equals("Exit")){
-            JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?");
+        if(amendBook.getModel().isPressed()){
+            JOptionPane.showConfirmDialog(null, "Are you sure you want to amend this book?", "Are You Sure", JOptionPane.YES_NO_OPTION);
 
         }
-
-
-
-
-    }
-
-    private void amendBookFrame() {
-        JFrame jFrame = new JFrame();
-        String header = "Amend Book";
-        jFrame.setTitle(header);
-        jFrame.setSize(500,500);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setVisible(true);
-
 
 
 
@@ -349,52 +405,66 @@ public class LibraryGUI extends JFrame implements ActionListener {
         }
 
 
-
-
     }
 
 
     private void addCustomerFrame() {
 
         JFrame jFrame = new JFrame();
-        String header = "Add Customer";
+        String header = "Add Book";
         jFrame.setTitle(header);
-        jFrame.setSize(500,500);
+        jFrame.setSize(500, 500);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
         JPanel jp = new JPanel();
+        jp.setLayout(new GridLayout(0, 1));
         jFrame.add(jp);
         JLabel name = new JLabel("Name");
+        name.setPreferredSize(new Dimension(10, 15));
+        name.setVerticalAlignment(JLabel.BOTTOM);
         jp.add(name);
         JTextField jtf = new JTextField();
-        jtf.setBounds(110,130,80,40);
+        jtf.setPreferredSize(new Dimension(50, 10));
+        //jtf.setBounds(110,130,80,40);
         jtf.addActionListener(this);
         jp.add(jtf);
 
 
         JLabel address = new JLabel("Address");
+        address.setVerticalAlignment(JLabel.BOTTOM);
         jp.add(address);
         jtf = new JTextField();
-        jtf.setBounds(110,130,80,40);
+        address.setPreferredSize(new Dimension(10, 15));
+
         jtf.addActionListener(this);
         jp.add(jtf);
 
 
-        JLabel isbn = new JLabel("ISBN");
-        jp.add(isbn);
+        JLabel pps = new JLabel("PPS");
+        jp.add(pps);
         jtf = new JTextField();
-        jtf.setBounds(110,130,80,40);
+        pps.setPreferredSize(new Dimension(10, 15));
+        jtf.addActionListener(this);
+        jp.add(jtf);
+
+        JLabel phone = new JLabel("Phone");
+        jp.add(phone);
+        jtf = new JTextField();
+        phone.setPreferredSize(new Dimension(10, 15));
         jtf.addActionListener(this);
         jp.add(jtf);
 
         JButton addCustomer = new JButton("Add Customer");
+
         jp.add(addCustomer);
 
-        if(addCustomer.getModel().isPressed()){
+        if (addCustomer.getModel().isPressed()) {
             JOptionPane.showConfirmDialog(null, "Are you sure you want to add a customer?", "Are You Sure", JOptionPane.YES_NO_OPTION);
+
+            //addBook.add(b);
+
+
         }
-
-
     }
 
 
@@ -405,6 +475,53 @@ public class LibraryGUI extends JFrame implements ActionListener {
         jFrame.setSize(500,500);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
+
+        JPanel jp = new JPanel();
+        jFrame.add(jp);
+        JComboBox bookList = new JComboBox<>();
+        bookList.addActionListener(this);
+        bookList.setVisible(true);
+        JLabel name = new JLabel("Name");
+        name.setVerticalAlignment(JLabel.BOTTOM);
+        jp.add(name);
+        JTextField jtf = new JTextField();
+        jtf.setSize(1150,100);
+        jtf.setBounds(110,130,80,40);
+        jtf.addActionListener(this);
+        jp.add(jtf);
+
+
+        JLabel address = new JLabel("Address");
+        address.setVerticalAlignment(JLabel.BOTTOM);
+        jp.add(address);
+        jtf = new JTextField();
+        //jtf.setBounds(110,130,80,40);
+        jtf.addActionListener(this);
+        jp.add(jtf);
+
+
+        JLabel isbn = new JLabel("ISBN");
+        jp.add(isbn);
+        jtf = new JTextField();
+        //jtf.setBounds(110,130,80,40);
+        jtf.addActionListener(this);
+        jp.add(jtf);
+
+        JLabel phone = new JLabel("Phone");
+        jp.add(phone);
+        jtf = new JTextField();
+        jtf.setBounds(110,130,80,40);
+        jtf.addActionListener(this);
+        jp.add(jtf);
+
+        JButton addBook = new JButton("Amend Book");
+
+        jp.add(addBook);
+
+        if(addBook.getModel().isPressed()){
+            JOptionPane.showConfirmDialog(null, "Are you sure you want to amend this book?", "Are You Sure", JOptionPane.YES_NO_OPTION);
+
+        }
 
 
 
